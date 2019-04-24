2e7c869ed43cc2a24463e6aefce117d49e2bc64c
brunogrbr <brunogoya.r@gmail.com>
2019-04-24 10:46:45 -0300

    Initial commit

---
+++ WeatherForeCastByCity/app/src/main/java/bruno/com/br/weatherforecastbycity/MainActivity.java
@@ -0,0 +1,130 @@
+package bruno.com.br.weatherforecastbycity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView weatherRecyclerView;
+    private WeatherAdapter adapter;
+    private List<Weather> previsoes;
+    private RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        weatherRecyclerView =
+                findViewById(R.id.weatherRecyclerView);
+        previsoes = new ArrayList<>();
+        adapter = new WeatherAdapter(previsoes, this);
+        weatherRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        weatherRecyclerView.setAdapter(adapter);
+        requestQueue = Volley.newRequestQueue(this);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void obtemPrevisoes (String cidade){
+        String url = getString(
+                R.string.web_service_url,
+                cidade,
+                getString(R.string.api_key)
+        );
+        JsonObjectRequest req = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                (response)->{
+                    previsoes.clear();
+                    try{
+                        JSONArray list = response.getJSONArray("list");
+                        for (int i = 0; i < list.length(); i++){
+                            JSONObject day = list.getJSONObject(i);
+                            JSONObject main = day.getJSONObject("main");
+                            JSONObject weather = day.getJSONArray("weather").getJSONObject(0);
+                            previsoes.add (new Weather(day.getLong("dt"), main.getDouble("temp_min"),
+                                    main.getDouble("temp_max"), main.getDouble ("humidity"),
+                                    weather.getString("description"),weather.getString("icon")));
+                        }
+                        adapter.notifyDataSetChanged();
+                        dismissKeyboard(weatherRecyclerView);
+
+                    }
+                    catch (JSONException e){
+                        e.printStackTrace();
+                    }
+                },
+                (error)->{
+                    Toast.makeText(
+                            MainActivity.this,
+                            getString(R.string.connect_error) + ": " + error.getLocalizedMessage(),
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+        );
+        requestQueue.add(req);
+    }
+    private void dismissKeyboard (View view){
+        InputMethodManager imm = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+    }
+
+
+}
